34384415bda91814b8ffb2d1ff212df3e9d17fde
yehudis raitman <yehudisr@gmail.com>
2021-01-19 16:24:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,10 +16,10 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   [
-    { name:  "Don Gately", occupation: "Live-in Staff" }
-     { name:  "Joelle van Dyne", occupation: "Radio Personality"}
-    { name:  "Pat Monteseian", occupation: "Staff" }
-    { name:  "Kate Gompert", occupation: "None" }
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+     { name:  "Joelle van Dyne", occupation: "Radio Personality"},
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
